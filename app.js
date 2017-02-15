87f7dd8ffdb88b8e183a551070473aff63174e34
alter-sachin <guitarplayersachin@gmail.com>
2017-02-15 22:30:01 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// BASE SETUP
+// ========================================================
+
+//call the packages we need
+var express = require("express");	//call express
+var app = express();	//define our app using express
+var bodyParser = require("body-parser");
+
+
+
+var port = 3000;
+
+
+var router = express.Router();
+
+//middleware to use for all requests
+router.use(function(req,res,next){
+	//log (Do analytics for all request , do a common task like verification here)
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	console.log("A request came:- " + fullUrl);
+	next(); //make sure we go to the next routes and not stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://ip_address/api/)
+router.get("/",function(req,res){
+	res.json({message: 'hello world !!!!'});
+});
+
+
+//START THE SERVER
+// ======================================================
+app.listen(port);
+console.log("Server running at port :- " + port);
+
+
+//
