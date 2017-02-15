e177927be4bb2f5be7071de111c9681124c4339c
Raman Guleria <rguleria@expedia.com>
2017-02-15 23:14:02 +0530

    Adding winston

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var express = require("express");	//call express
 var app = express();	//define our app using express
 var bodyParser = require("body-parser");
+const winston = require('winston')
 
 
 
